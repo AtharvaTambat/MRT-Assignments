d2c148cf138be80fbe0353728990b7fedfe3926c
AtharvaTambat <tambat.atharva@gmail.com>
2022-07-13 13:21:57 +0530

    removing all files

--- joystick_assignment/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atharva/joystick_assignment/src
-# Build directory: /home/atharva/joystick_assignment/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("joystick")
--- joystick_assignment/build/joystick/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atharva/joystick_assignment/src/joystick
-# Build directory: /home/atharva/joystick_assignment/build/joystick
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
