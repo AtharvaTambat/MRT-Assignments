dc97b43c1c2ccb0e8e5403cbfe5834c4fd208219
AtharvaTambat <tambat.atharva@gmail.com>
2022-07-22 03:26:53 +0530

    Deleting Assignment 2

--- gazebo_assignment/build/gazebo_envs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atharva/gazebo_assignment/src/gazebo_envs
-# Build directory: /home/atharva/gazebo_assignment/build/gazebo_envs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gazebo_assignment/build/pysdf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atharva/gazebo_assignment/src/pysdf
-# Build directory: /home/atharva/gazebo_assignment/build/pysdf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
