c24f87b0795586676f6b83f6be92359fc6edc145
AtharvaTambat <tambat.atharva@gmail.com>
2022-07-22 03:19:47 +0530

    Deleting Assignment 2

--- Assignment 2 - Gazebo/build/bot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atharva/gazebo_assignment/src/bot_description
-# Build directory: /home/atharva/gazebo_assignment/build/bot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
