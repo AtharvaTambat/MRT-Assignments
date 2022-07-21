3719827c144c970049283b6e4c02c450f4f55762
AtharvaTambat <tambat.atharva@gmail.com>
2022-07-22 03:30:16 +0530

    Adding Assignment 2

---
+++ Assignment 2 - Gazebo/build/bot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/atharva/gazebo_assignment/src/bot_description
+# Build directory: /home/atharva/gazebo_assignment/build/bot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Assignment 2 - Gazebo/build/gazebo_envs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/atharva/gazebo_assignment/src/gazebo_envs
+# Build directory: /home/atharva/gazebo_assignment/build/gazebo_envs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
